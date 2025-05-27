920190728932e618ef43f2bcff49f36d6f7eb8f5
Mohamed Achraf Aneflous <achrafanaflous99@gmail.com>
2025-05-27 14:34:15 +0100

    All files for homestay website

---
+++ java.js
@@ -0,0 +1,8 @@
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.onclick = function(e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+    };
+  });
+  
